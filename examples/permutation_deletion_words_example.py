b8e45814744fddc09d5c4473ca9f04b6bb0500cc
Anton <zakladniy.a@mail.ru>
2022-11-05 17:56:33 +0300

    Add permutation and deletion words class

--- examples/permutation_deletion_words_example.py
+++ examples/permutation_deletion_words_example.py
@@ -1,19 +1,21 @@
-# import time
-#
-# from text_data_augmentation_tools.permutation_deletion_words import PermDropWord
-#
-# if __name__ == '__main__':
-#
-#     text = "добрый день, подскажите пожалуйста как пройти в библиотеку?"
-#
-#     per_drop_word = PermDropWord()
-#
-#     # Drop random words
-#     start_time = time.time()
-#     print(per_drop_word.drop_random_word(text=text, n_drops=1))
-#     print(f"Exec time, s: {(time.time() - start_time)}")
-#
-#     # Swap words
-#     start_time = time.time()
-#     print(per_drop_word.swap_words(text=text))
-#     print(f"Exec time, s: {(time.time() - start_time)}")
+import time
+
+from text_data_augmentation_tools.permutation_deletion_words import (
+    PermDropWord
+)
+
+if __name__ == '__main__':
+
+    text = "добрый день, подскажите пожалуйста как пройти в библиотеку?"
+
+    per_drop_word = PermDropWord()
+
+    # Drop random words
+    start_time = time.time()
+    print(per_drop_word.drop_random_word(text=text, n_drops=1))
+    print(f"Exec time, s: {(time.time() - start_time)}\n\n")
+
+    # Swap words
+    start_time = time.time()
+    print(per_drop_word.swap_words(text=text))
+    print(f"Exec time, s: {(time.time() - start_time)}")
